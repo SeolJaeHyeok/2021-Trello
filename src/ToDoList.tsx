1bd40a76e41204335e54c7479bf6f2a333453448
Jaehyeok <milkboy2564@naver.com>
2021-12-13 03:33:17 +0900

    react-hook-form
    1) react-hook-form의 register와 watch
    - register를 사용하면 onChange 이벤트와 state(사용자의 입력)를 설정할 수 있고 watch함수를 사용하여 state를 추적, 관찰할 수 있다.

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function ToDoList() {
+/* function ToDoList() {
   const [todo, setTodo] = useState("");
   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
     const {
@@ -19,6 +20,28 @@ function ToDoList() {
           onChange={onChange}
           value={todo}
           placeholder="Write to do"
+        ></input>
+        <button>Add</button>
+      </form>
+    </div>
+  );
+} */
+
+function ToDoList() {
+  const { register, watch } = useForm();
+  console.log(register("todo"));
+  console.log(watch());
+  return (
+    <div>
+      <form>
+        <input {...register("Email")} placeholder="Email"></input>
+        <input {...register("firstName")} placeholder="firstName"></input>
+        <input {...register("lastName")} placeholder="lastName"></input>
+        <input {...register("userName")} placeholder="userName"></input>
+        <input {...register("password")} placeholder="password"></input>
+        <input
+          {...register("passwordConfirmation")}
+          placeholder="passwordConfirmation"
         ></input>
         <button>Add</button>
       </form>
