225817d3bce7685257b9a15c4141babbbb367ae1
Jaehyeok <milkboy2564@naver.com>
2021-12-14 04:08:05 +0900

    To Do List - Selector
    1) Recoil - selector
    - selector는 atom의 반환값을 변경시키는 함수를 의미한다.
    - selector의 get 함수를 이용하여 atom(state)를 selector 안으로 가져올 수 있다. 
    - selector가 가져온 state를 가지고 현재 category에 맞는 list만 반환하여 화면에 렌더링
    - 카테고리를 옮긴 뒤 To Do를 추가하면 기본값으로 지정된 "TO_DO" 카테고리에 추가가 된다. 
    위와 같이 추가되는 것보다 옮긴 카테고리 안에 바로 추가가 되게끔 만들어야 함.

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface IToDo {
   text: string;
@@ -6,7 +6,23 @@ export interface IToDo {
   category: "TO_DO" | "DOING" | "DONE";
 }
 
+export const categoryState = atom({
+  key: "category",
+  default: "TO_DO",
+});
+
 export const toDoState = atom<IToDo[]>({
   key: "toDos",
   default: [],
+});
+
+// selector는 atom의 반환값을 변형시키는 함수를 의미한다.
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    const category = get(categoryState);
+    // 현재 카테고리와 맞는 toDo list만 반환
+    return toDos.filter((toDo) => toDo.category === category);
+  },
 });
