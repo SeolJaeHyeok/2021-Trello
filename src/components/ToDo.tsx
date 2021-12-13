ed7d2dc814599a328008145bf4ca8701f24f0b3d
Jaehyeok <milkboy2564@naver.com>
2021-12-13 19:19:25 +0900

    To Do List - Get argument
    1) 버튼을 눌렀을 때 어떠한 버튼을 눌렀는지 확인하기
    - 버튼 안에서 원하는 정보를 가져오기 위해서는 두 가지의 방법이 있다.
    1. 인자를 받는 함수를 만들고 버튼 안의 이벤트 리스너에서 익명함수를 호출하여 인자를 명시적으로 전달해주는 방법
    2. 버튼에 name 어트리뷰트를 추가해준 다음 이벤트 리스너를 통해 해당 이벤트 객체에서 원하는 정보를 추출하는 방법

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,38 @@
-import { IToDo } from "../atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    console.log(name);
+  };
+  /* 
+  인자를 HTML 요소 안에서 가져오는 방법
+  1. 인자를 넘겨받는 함수를 만들고 새 익명함수를 선언하여 인자를 넘겨주는 방법
+  2. name 어트리뷰트를 추가한 다음 이벤트 객체 안에서 name을 추출하는 방법
+  */
   return (
     <li>
-      {text}
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      <span>{text}</span>
+      {category !== "DOING" && (
+        <button name="Doing" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
